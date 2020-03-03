256d5077d7b255195249e6e51877725e1900d01f
DaeHun <skypedanny@naver.com>
2020-03-03 09:01:12 +0900

    [CREATE] Create Benefit Component & Connect MainContainer

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "components/Header/Header";
 import Welcome from "components/Welcome/Welcome";
 import About from "components/About/About";
+import Benefit from "components/Benefit/Benefit";
 
 class MainContainer extends Component {
   render() {
@@ -10,6 +11,7 @@ class MainContainer extends Component {
         <Header />
         <Welcome />
         <About />
+        <Benefit />
       </>
     );
   }
