b452bb32d9e3935b00520c028fbae657b0ffc5ce
DaeHun <skypedanny@naver.com>
2020-03-04 11:08:25 +0900

    [UPDATE] Modify Welcome Component

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./Welcome.scss";
 
-//import { ReactComponent as Clip } from "assets/Clip.svg";
 import Clip from "assets/Clip.png";
 
 class Welcome extends Component {
