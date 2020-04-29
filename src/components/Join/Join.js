19618f50d8d1038cb4b6baada0dd884d72ad2397
daehun1 <skypedanny@naver.com>
2020-04-29 22:28:15 +0900

    [CREATE] Create Join Component

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -15,13 +15,33 @@ class Join extends Component {
       <div className="c-join">
         <div className="c-join__wrapper">
           <div className="c-join__wrapper--title">Apply</div>
-          <span>2020. 03. 21 ~ 2020. 03. 29</span>
+          <span className="c-join__wrapper--term">2020. 03. 21 ~ 2020. 03. 29</span>
           <div className="c-join__wrapper--first">
             <div className="c-join__wrapper--first__number">
               <span>학번</span>
-              
+              <input></input>
+            </div>
+            <div className="c-join__wrapper--first__name">
+              <span>이름</span>
+              <input></input>
             </div>
           </div>
+
+          <div className="c-join__wrapper--second">
+            <span>지원분야</span>
+            <div className="c-join__wrapper--second__type">
+              <button>Design</button>
+              <button>Front - End</button>
+              <button>Back - End</button>
+            </div>
+          </div>
+
+          <div className="c-join__wrapper--third">
+            <span>지원동기</span>
+            <textarea rows="20" cols="30"></textarea>
+          </div>
+
+          <button>SEND</button>
         </div>
       </div>
     );
