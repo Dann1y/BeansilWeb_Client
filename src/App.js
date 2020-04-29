6c38585b961b144f9f42fe21763f964a403d9375
daehun1 <skypedanny@naver.com>
2020-04-29 11:11:40 +0900

    [UPDATE] Add JoinContainer in App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import MainContainer from "containers/MainContainer/MainContainer";
 import TeamContainer from "containers/TeamContainer/TeamContainer";
+import JoinContainer from "containers/JoinContainer/JoinContainer";
 
 const RoutesComponent = () => (
   <Router>
     <Route exact path="/" component={MainContainer} />
     <Switch>
       <Route path="/introduce" component={TeamContainer} />
+      <Route path="/joinus" component={JoinContainer} />
     </Switch>
   </Router>
 );
