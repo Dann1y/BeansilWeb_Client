7435c3549463dac247e92d99ffe19d8b8ac61499
daehun1 <skypedanny@naver.com>
2020-05-08 23:31:11 +0900

    [UPDATE] Modify Header Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,13 +7,13 @@ class Header extends Component {
   render() {
     return (
       <div className="c-header">
-        <Logo />
+        <Logo onClick={"/"}/>
         <div className="c-header__wrapper">
           <span className="c-header__wrapper--menu">ABOUT</span>
           <span className="c-header__wrapper--menu">SKILL</span>
           <span className="c-header__wrapper--menu">APPLY</span>
-          <span className="c-header__wrapper--menu">REVIEW</span>
           <span className="c-header__wrapper--menu">TEAM</span>
+          <span className="c-header__wrapper--menu">Log-In</span>
         </div>
       </div>
     );
